a5c93c883a1fe9e9b6e9d0f1a8ee91bb09eff50d
Andreas Huber <andreas.huber@lawo.com>
2015-09-23 14:10:32 +0200

    Run tests with the VS14.0 console

--- RunTests.bat
+++ RunTests.bat
@@ -1,7 +1,7 @@
 rmdir TestResults /s /q
 rmdir CoverageResults /s /q
 
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" Lawo.EmberPlusTest\bin\Release\Lawo.EmberPlusTest.dll LawoTest\bin\Release\LawoTest.dll /InIsolation /TestCaseFilter:"TestCategory=Unattended" /EnableCodeCoverage /Settings:CodeCoverage.runsettings /Logger:trx
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" Lawo.EmberPlusTest\bin\Release\Lawo.EmberPlusTest.dll LawoTest\bin\Release\LawoTest.dll /InIsolation /TestCaseFilter:"TestCategory=Unattended" /EnableCodeCoverage /Settings:CodeCoverage.runsettings /Logger:trx
 
 mkdir CoverageResults
 move TestResults\*.trx CoverageResults\TestResults.trx
