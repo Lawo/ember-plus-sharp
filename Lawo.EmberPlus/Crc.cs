8c974448ca3a0d6b897d844af2275f0e687c81b7
Andreas Huber <andreas.huber@lawo.com>
2015-10-30 16:04:44 +0100

    Disable the Log Folder TextBox while the proxy is running

--- Lawo.EmberPlus/Crc.cs
+++ Lawo.EmberPlus/Crc.cs
@@ -4,7 +4,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-namespace Lawo
+namespace Lawo.EmberPlus
 {
     internal static class Crc
     {
