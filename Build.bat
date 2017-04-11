0cee985ba7876f3e53ba8b5691dbbb0d9774ff89
Andreas Huber <andreas.huber@lawo.com>
2017-04-11 18:20:32 +0200

    Revert changes to fix #41

--- Build.bat
+++
@@ -1,4 +0,0 @@
-powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile '%~dp0nuget.exe'"
-"%~dp0nuget.exe" restore "%~dp0Lawo.EmberPlusSharp.sln"
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Lawo.EmberPlusSharp.sln /t:Clean /p:Configuration=Release
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Lawo.EmberPlusSharp.sln /t:Build /p:Configuration=Release
